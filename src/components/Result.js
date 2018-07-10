7d987c1c207186471c455cf87300041721f2b351
kyun <lsk9105@gmail.com>
2018-07-10 11:56:48 +0900

    finish render 완료 / section 때문에 flex 깨짐

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './Result.css';
+import React, { Component } from "react";
+import "./Result.css";
 
 class Result extends Component {
   // strike, ball 갯수 만큼 ● 찍어주기
   // Todo : defaultProps 설정, 함수형 컴포넌트로 변경
   dotCount = inputCount => {
-    let dot = '';
+    let dot = "";
     for (let i = 0; i < inputCount; i++) {
-      dot += ' ●';
+      dot += " ●";
     }
     return dot;
   };
@@ -18,21 +18,25 @@ class Result extends Component {
     const strikeCount = this.dotCount(strike);
     const ballCount = this.dotCount(ball);
 
-    console.log(strikeCount);
+    //! section tag 때문에 flex 깨짐
     return (
       <div className="result">
         <div className="result-count">{count}) </div>
         <div className="result-value">{value}</div>
-        <div className="result-strike">
-          Strike : <span className="strikeCount">{strikeCount}</span>
-        </div>
-        <div className="result-ball">
-          ball : <span className="ballCount">{ballCount}</span>
-        </div>
-        {
-          finish && (<h1> Finish </h1>) 
-        }
-
+        {finish ? (
+          <section>
+            <div className="result-finish">Finish</div>
+          </section>
+        ) : (
+          <section>
+            <div className="result-strike">
+              Strike : <span className="strikeCount">{strikeCount}</span>
+            </div>
+            <div className="result-ball">
+              ball : <span className="ballCount">{ballCount}</span>
+            </div>
+          </section>
+        )}
       </div>
     );
   }
