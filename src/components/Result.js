0cbe33137235e1f3df794dc8c8350d695abd9a86
kyun <lsk9105@gmail.com>
2018-07-16 10:59:56 +0900

    Result 수정

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import './Result.css';
+import React from "react";
+import "./Result.css";
 
-class Result extends Component {
+const Result = ({ input }) => {
+  const { strike, ball, finish, value, count } = input;
   // strike, ball 갯수 만큼 ● 찍어주기
-  dotCount = inputCount => {
-    let dot = '';
+  const dotCount = (inputCount) =>{
+    let dot = "";
     for (let i = 0; i < inputCount; i++) {
-      dot += ' ●';
+      dot += " ●";
     }
     return dot;
   };
-  render() {
-    const { value, strike, ball, count, finish } = this.props.input;
 
-    const strikeCount = this.dotCount(strike);
-    const ballCount = this.dotCount(ball);
-    return (
-      <div className={`result ${finish && 'finish'}`}>
-        <div className="result-count">{count}) </div>
-        <div className="result-value">{value}</div>
+  let strikeCount = dotCount(strike);
+  let ballCount = dotCount(ball);
 
-        {finish ? (
-          <section className="result-finish">
-            <div>Finish</div>
-          </section>
-        ) : (
-          <section className="result-section-count">
-            <div className="result-strike">
-              Strike : <span className="strikeCount">{strikeCount}</span>
-            </div>
-            <div className="result-ball">
-              ball : <span className="ballCount">{ballCount}</span>
-            </div>
-          </section>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={`result ${finish && "finish"}`}>
+      <div className="result-count">{count}) </div>
+      <div className="result-value">{value}</div>
+
+      {finish ? (
+        <section className="result-finish">
+          <div>Finish</div>
+        </section>
+      ) : (
+        <section className="result-section-count">
+          <div className="result-strike">
+            Strike : <span className="strikeCount">{strikeCount}</span>
+          </div>
+          <div className="result-ball">
+            ball : <span className="ballCount">{ballCount}</span>
+          </div>
+        </section>
+      )}
+    </div>
+  );
+};
 
 export default Result;
